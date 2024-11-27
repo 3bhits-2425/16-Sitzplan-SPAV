0bb9565e8bd33114a69ca5e79883c2a313289026
VSPAJIC <viktor.spajic07@Htl-salzburg.ac.at>
2024-11-27 09:09:59 +0100

    V1

--- Assets/MyGame/Scripts/StudentData.cs
+++ Assets/MyGame/Scripts/StudentData.cs
@@ -6,14 +6,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewStudent", menuName = "Seatplan/Student", order = 1)]
 public class StudentData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        public string studentName;
+    public string studentName;
     public Color eyecolor;
-        public Sprite studentImage;
+    public Sprite studentImage;
     public AudioClip studentClip;
-    }
 }
 
     
